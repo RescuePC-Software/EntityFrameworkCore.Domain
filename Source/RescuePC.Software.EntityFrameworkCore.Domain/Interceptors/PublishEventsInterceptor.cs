228049c196ca37cb8d996ebc98f691b5821b77c4
Wiktor Wijata <wiktorwijata@gmail.com>
2026-08-12 09:27:02 +0200

    init commit

---
+++ Source/RescuePC.Software.EntityFrameworkCore.Domain/Interceptors/PublishEventsInterceptor.cs
@@ -0,0 +1,37 @@
+﻿namespace RescuePC.Software.EntityFrameworkCore.Domain.Interceptors;
+
+public class PublishEventsInterceptor : SaveChangesInterceptor
+{
+    private readonly IEventBus _eventBus;
+
+    public PublishEventsInterceptor(IEventBus eventBus)
+    {
+        _eventBus = eventBus;
+    }
+
+    public override async ValueTask<int> SavedChangesAsync(
+        SaveChangesCompletedEventData eventData,
+        int result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is null) return await base.SavedChangesAsync(eventData, result, cancellationToken);
+        var aggregateRoots = eventData.Context.ChangeTracker
+            .Entries<IAggregateRoot>()
+            .Select(e => e.Entity)
+            .Where(e => e.GetDomainEvents().Count > 0)
+            .ToList();
+
+        var events = aggregateRoots
+            .SelectMany(e => e.GetDomainEvents())
+            .ToList();
+
+        aggregateRoots.ForEach(e => e.ClearDomainEvents());
+
+        foreach (var @event in events)
+        {
+            await _eventBus.PublishAsync(@event, cancellationToken);
+        }
+
+        return await base.SavedChangesAsync(eventData, result, cancellationToken);
+    }
+}
